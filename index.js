7d639701937fb556bd64acde678443651ef0a9dc
Mayo <mayolito.waje08@gmail.com>
2022-11-29 12:39:03 +0800

    add routes

--- index.js
+++ index.js
@@ -7,23 +7,36 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'));
 
+app.get('/api/notes/:id', async (req, res, next) => {
+  const foundNote = await Note.findById(req.params.id);
+  if (!foundNote) {
+    next();
+  } else {
+    res.json(foundNote);
+  }
+});
+
+app.put('/api/notes/:id', async (req, res) => {
+  const updatedNote = await Note.findByIdAndUpdate(req.params.id, req.body);
+  res.json(updatedNote);
+});
+
 app.post('/api/notes', async (req, res) => {
   const newNote = req.body.content;
-  const isImportant = req.body.important;
 
-  if (!newNote || isImportant !== true && isImportant !== false) {
+  if (!newNote) {
     res.status(400).json({
-      error: "The request body should contain content (string) and important (boolean) field"
+      error: "The request body should contain content (string)"
     })
   } else {
     const note = new Note({
       content: newNote,
       date: new Date(),
-      important: isImportant,
+      important: req.body.important || false,
     });
   
-    await note.save();
-    res.status(201).json(note);
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
   }
 });
 
@@ -31,6 +44,12 @@ app.get('/api/notes', async (req, res) => {
   const notes = await Note.find({});
   res.json(notes);
 });
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Resource Not Found (404)"
+  });
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server is listening on port ", PORT));
