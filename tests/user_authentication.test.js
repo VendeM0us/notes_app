db40f82d4697d7dc5f5557b891f351f54c9386b7
Mayo <mayolito.waje08@gmail.com>
2022-12-05 10:15:50 +0800

    improve test for user creation

--- tests/user_authentication.test.js
+++ tests/user_authentication.test.js
@@ -1,26 +1,46 @@
 import mongoose from 'mongoose';
 import supertest from 'supertest';
+import bcrypt from 'bcrypt';
 import app from '../app.js';
+import User from '../models/user.js';
+import * as helper from './test_helper.js';
 
 const api = supertest(app);
 
+beforeEach(async () => {
+  await User.deleteMany({});
+
+  const passwordHash = await bcrypt.hash('secret', 10);
+  const user = new User({
+    username: 'init',
+    name: 'init',
+    passwordHash,
+  });
+
+  await user.save();
+}, 100000);
+
 describe('create new user', () => {
   test('should create new user', async () => {
+    const usersAtStart = await helper.usersInDb();
+
     const newUser = {
       username: 'testUser',
       name: 'testName',
       password: 'testPassword8391!',
     };
 
-    const createdUser = await api
+    await api
       .post('/api/users')
       .send(newUser)
       .expect(201)
       .expect('Content-Type', /application\/json/);
 
-    expect(createdUser.body.username).toBe('testUser');
-    expect(createdUser.body.name).toBe('testName');
-    expect(createdUser.body.passwordHash).not.toBeDefined();
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map(u => u.username);
+    expect(usernames).toContain(newUser.username);
   });
 });
 
