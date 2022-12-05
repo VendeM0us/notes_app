118f80c4e913e3c1e11b790e4b0e26673c94fa91
Mayo <mayolito.waje08@gmail.com>
2022-12-05 21:07:11 +0800

    update notes creation functionality

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Note from '../models/note.js';
+import User from '../models/user.js';
 
 const notesRouter = express.Router();
 
@@ -26,21 +27,27 @@ notesRouter.put('/:id', async (req, res, next) => {
   res.json(updatedNote);
 });
 
-notesRouter.delete('/:id', async (req, res, next) => {
+notesRouter.delete('/:id', async (req, res) => {
   await Note.findByIdAndDelete(req.params.id);
   res.status(204).send();
 });
 
-notesRouter.post('/', async (req, res, next) => {
+notesRouter.post('/', async (req, res) => {
   const body = req.body;
+
+  const user = await User.findById(body.userId);
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
     date: new Date(),
+    user: user._id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   res.status(201).json(savedNote);
 });
 
