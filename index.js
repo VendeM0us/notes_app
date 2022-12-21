7fdfed277dbe032fadfc421b63941df14764f0b8
Mayo <mayolito.waje08@gmail.com>
2022-12-21 13:52:53 +0800

    update loggers and add cypress

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import app from './app.js';
 import http from 'http';
 import * as config from './utils/config.js';
-import * as logger from './utils/logger.js';
 
 const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
-  logger.info('Server is running at port ', config.PORT);
+  console.log('Server is listening at port ', config.PORT);
 });
